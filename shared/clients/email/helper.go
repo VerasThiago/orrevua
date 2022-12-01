4f09386407aaefedd7395d63bef59c573c442db5
Thiago Veras <github@verasthiago.com>
2022-12-01 13:34:48 -0300

    Feat/send invite qrcode email (#13)

---
+++ shared/clients/email/helper.go
@@ -0,0 +1,91 @@
+package email
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"net/smtp"
+	"strings"
+	"text/template"
+
+	"github.com/verasthiago/tickets-generator/shared/models"
+)
+
+const (
+	QR_CODE_TEMPLATE_PATH = "../shared/templates/email.html"
+	INVITE_TITLE          = "Despedida Veras - Invite"
+	TICKETS_TILE          = "Despedida Veras - Ingressos"
+	TICKETS_BODY          = "Aqui está seus ingressos, tenha uma boa festa!"
+	PNG_EXTENTION         = "png"
+)
+
+func (s *SMTP) generateAttachmentsBody(email models.Email) []byte {
+
+	buf := bytes.NewBuffer(nil)
+	withAttachments := len(email.Attachments) > 0
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", email.Title))
+	buf.WriteString(fmt.Sprintf("To: %s\n", []string{email.To}))
+	buf.WriteString("MIME-Version: 1.0\n")
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=%s\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+	} else {
+		buf.WriteString("Content-Type: text/plain; charset=utf-8\n")
+	}
+
+	buf.WriteString(email.Body)
+
+	if withAttachments {
+		for k, v := range email.Attachments {
+			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
+		}
+
+		buf.WriteString("--")
+	}
+
+	return buf.Bytes()
+}
+
+func (s *SMTP) sendEmailWithAttachments(email models.Email) error {
+	addr := fmt.Sprintf("%+v:%+v", s.host, s.port)
+	return smtp.SendMail(addr, s.auth, s.srcEmail, []string{email.To}, s.generateAttachmentsBody(email))
+
+}
+
+func (s *SMTP) sendHtmlEmail(email models.Email) error {
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: " + email.Title + "\n"
+	msg := []byte(subject + mime + "\n" + email.Body)
+	addr := fmt.Sprintf("%+v:%+v", s.host, s.port)
+
+	return smtp.SendMail(addr, s.auth, s.srcEmail, []string{email.To}, msg)
+}
+
+func parseTemplate(data interface{}) (*string, error) {
+	t, err := template.ParseFiles(QR_CODE_TEMPLATE_PATH)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return nil, err
+	}
+	res := buf.String()
+	return &res, err
+}
+
+func addFileExtention(fileName, fileExtention string) string {
+	return strings.Join([]string{fileName, fileExtention}, ".")
+}
