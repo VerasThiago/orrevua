7dd4341053386d1307643bcd20bb16353b985c6a
verasthiago <github@verasthiago.com>
2022-12-04 03:48:19 -0300

    Generic error model

--- shared/errors/errors.go
+++ shared/errors/errors.go
@@ -1,29 +1,64 @@
-package error
+package errors
 
 import (
-	"net/http"
+	"fmt"
+	"strconv"
 
+	"github.com/bugsnag/bugsnag-go"
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
-	"gorm.io/gorm"
+	log "github.com/sirupsen/logrus"
 )
 
-func HandleInternalServerError(context *gin.Context, err error, log *zap.Logger) {
-	if err == gorm.ErrRecordNotFound {
-		context.JSON(http.StatusNotFound, gin.H{"error": "object not found on database"})
-	} else {
-		log.Sugar().Errorw(err.Error())
-		context.JSON(http.StatusInternalServerError, gin.H{"error": "unexpected behavior"})
+type JsonError interface {
+	Error() string
+	GenerateJsonResponse(c *gin.Context)
+}
+
+type MetaData map[string]interface{}
+
+type GenericError struct {
+	Code     StatusCode
+	Err      error `json:"-"`
+	Message  string
+	MetaData MetaData `json:"-"`
+}
+
+func (e GenericError) Error() string {
+	return fmt.Sprintf("Code:%v\nOriginal error:%v\nMetaData:%v\nMessage:%v",
+		e.Code, e.Err, e.MetaData, e.Message)
+}
+
+func (e GenericError) GenerateJsonResponse(c *gin.Context) {
+	// if Err was provided log it
+	if e.Err != nil {
+		if e.MetaData == nil {
+			e.MetaData = MetaData{}
+		}
+		// add useful metadata
+		e.MetaData["url"] = c.FullPath()
+		e.MetaData["query"] = c.Request.URL.Query()
+		e.MetaData["post"] = c.Request.PostForm
+
+		// get saved user id
+		userId := c.GetInt64("user_id")
+		e.MetaData["user_id"] = userId
+
+		if userId > 0 {
+			_ = bugsnag.Notify(e.Err, bugsnag.MetaData{
+				"data": e.MetaData,
+			}, bugsnag.User{
+				Id: strconv.FormatInt(userId, 10),
+			})
+		} else {
+			_ = bugsnag.Notify(e.Err, bugsnag.MetaData{
+				"data": e.MetaData,
+			})
+		}
+
+		log.Error(e.Err)
+		log.Error(e.MetaData)
 	}
-	context.Abort()
-}
 
-func HandleBadRequestError(context *gin.Context, err error) {
-	context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	context.Abort()
-}
-
-func HandleBadRequestErrors(context *gin.Context, errors []string) {
-	context.JSON(http.StatusBadRequest, gin.H{"error": errors})
-	context.Abort()
+	log.Errorf("Response %v: %v", e.Code, e.Message)
+	c.AbortWithStatusJSON(int(e.Code), gin.H{"status": "failed", "message": e.Message})
 }
