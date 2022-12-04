76ae5494d3e103d6a13077ff3232b2e7ae0a03b6
verasthiago <github@verasthiago.com>
2022-12-04 20:47:15 -0300

    Handle errors

--- shared/repository/postgresRepository/ticket.go
+++ shared/repository/postgresRepository/ticket.go
@@ -5,62 +5,42 @@ import (
 
 	"github.com/verasthiago/tickets-generator/shared/errors"
 	"github.com/verasthiago/tickets-generator/shared/models"
-	"gorm.io/gorm"
 )
+
+const TICKET_MODEL_NAME = "Ticket"
 
 func (p *PostgresRepository) GetTicketListByUserID(userID string) ([]*models.Ticket, error) {
 	var ticketList []*models.Ticket
-	if record := p.db.Where("owner_id = ?", userID).Find(&ticketList); record.Error != nil {
-		if record.Error == gorm.ErrRecordNotFound {
-			return nil, errors.GenericError{
-				Code:    errors.STATUS_NOT_FOUND,
-				Err:     record.Error,
-				Message: "Ticket not found",
-			}
-		}
-		return nil, record.Error
+	if err := errors.HandleDataNotFoundError(p.db.Where("owner_id = ?", userID).Find(&ticketList).Error, TICKET_MODEL_NAME); err != nil {
+		return nil, err
 	}
 	return ticketList, nil
 }
 
 func (p *PostgresRepository) GetTicketByID(id string) (*models.Ticket, error) {
 	var ticket *models.Ticket
-	if record := p.db.Where("id = ?", id).First(&ticket); record.Error != nil {
-		if record.Error == gorm.ErrRecordNotFound {
-			return nil, errors.GenericError{
-				Code:    errors.STATUS_NOT_FOUND,
-				Err:     record.Error,
-				Message: "Ticket not found",
-			}
-		}
-		return nil, record.Error
+	if err := errors.HandleDataNotFoundError(p.db.Where("id = ?", id).First(&ticket).Error, TICKET_MODEL_NAME); err != nil {
+		return nil, err
 	}
 	return ticket, nil
 }
 
 func (p *PostgresRepository) GetTicketByCPF(cpf string) (*models.Ticket, error) {
 	var ticket *models.Ticket
-	if record := p.db.Where("cpf = ?", cpf).First(&ticket); record.Error != nil {
-		if record.Error == gorm.ErrRecordNotFound {
-			return nil, errors.GenericError{
-				Code:    errors.STATUS_NOT_FOUND,
-				Err:     record.Error,
-				Message: "Ticket not found",
-			}
-		}
-		return nil, record.Error
+	if err := errors.HandleDataNotFoundError(p.db.Where("cpf = ?", cpf).First(&ticket).Error, TICKET_MODEL_NAME); err != nil {
+		return nil, err
 	}
 	return ticket, nil
 }
 
 func (p *PostgresRepository) CreateTicket(ticket *models.Ticket) error {
-	return p.db.Create(ticket).Error
+	return errors.HandleDuplicateError(p.db.Create(ticket).Error)
 }
 
 func (p *PostgresRepository) DeleteTicketByID(id string) error {
-	return p.db.Where("id = ?", id).Delete(&models.Ticket{}).Error
+	return errors.HandleDataNotFoundError(p.db.Where("id = ?", id).Delete(&models.Ticket{}).Error, TICKET_MODEL_NAME)
 }
 
 func (p *PostgresRepository) ValidateTicketByCPF(cpf string) error {
-	return p.db.Model(&models.Ticket{}).Where("cpf = ?", cpf).Updates(&models.Ticket{IsUsed: true, UsedTime: time.Now()}).Error
+	return errors.HandleDataNotFoundError(p.db.Model(&models.Ticket{}).Where("cpf = ?", cpf).Updates(&models.Ticket{IsUsed: true, UsedTime: time.Now()}).Error, TICKET_MODEL_NAME)
 }
