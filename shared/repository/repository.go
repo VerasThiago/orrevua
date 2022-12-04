8ce2a1cc740bd36610a375892ba9945f919b8a72
verasthiago <github@verasthiago.com>
2022-12-04 17:43:20 -0300

    Add ticket methods signature

--- shared/repository/repository.go
+++ shared/repository/repository.go
@@ -6,9 +6,17 @@ import (
 
 type Repository interface {
 	GetUserByEmail(email string) (*models.User, error)
+	GetUserByID(id string) (*models.User, error)
 	CreateUser(user *models.User) error
 	DeleteUser(userID string) error
 	UpdateUser(user *models.User) error
 	UpdateUserPasswordByEmail(email string, password string) error
 	VerifyUserAccountByID(id string) error
+
+	CreateTicket(ticket *models.Ticket) error
+	GetTicketListByUserID(userID string) ([]*models.Ticket, error)
+	GetTicketByID(id string) (*models.Ticket, error)
+	GetTicketByCPF(cpf string) (*models.Ticket, error)
+	DeleteTicketByID(id string) error
+	ValidateTicketByCPF(cpf string) error
 }
