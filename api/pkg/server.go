8d91ab04c8156d73cd7a339f2508bf1735206ea1
verasthiago <github@verasthiago.com>
2022-12-04 21:35:43 -0300

    Send invite route

--- api/pkg/server.go
+++ api/pkg/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/verasthiago/tickets-generator/api/pkg/builder"
 	"github.com/verasthiago/tickets-generator/api/pkg/handlers"
+	"github.com/verasthiago/tickets-generator/api/pkg/handlers/invite"
 	"github.com/verasthiago/tickets-generator/api/pkg/handlers/ticket"
 	"github.com/verasthiago/tickets-generator/api/pkg/middlewares"
 	"github.com/verasthiago/tickets-generator/shared/errors"
@@ -18,6 +19,8 @@ type Server struct {
 	TicketDelete   ticket.TicketDeleteAPI
 	TicketValidate ticket.TicketValidateAPI
 
+	InviteSend invite.InviteSendAPI
+
 	AuthAPI middlewares.AuthUserAPI
 }
 
@@ -28,6 +31,7 @@ func (s *Server) InitFromBuilder(builder builder.Builder) *Server {
 	s.TicketCreate = new(ticket.TicketCreateHandler).InitFromBuilder(builder)
 	s.TicketDelete = new(ticket.TicketDeleteHandler).InitFromBuilder(builder)
 	s.TicketValidate = new(ticket.TicketValidateHandler).InitFromBuilder(builder)
+	s.InviteSend = new(invite.InviteSendHandler).InitFromBuilder(builder)
 
 	s.AuthAPI = new(middlewares.AuthUserHandler).InitFromFlags(builder.GetFlags(), builder.GetSharedFlags())
 	return s
@@ -48,6 +52,10 @@ func (s *Server) Run() error {
 				ticketV0.POST("/create", errors.ErrorRoute(s.TicketCreate.Handler))
 				ticketV0.DELETE("/delete", errors.ErrorRoute(s.TicketDelete.Handler))
 				ticketV0.POST("/validate", errors.ErrorRoute(s.TicketValidate.Handler))
+			}
+			inviteVO := apiV0.Group("/invite")
+			{
+				inviteVO.POST("/send", errors.ErrorRoute(s.InviteSend.Handler))
 			}
 
 			apiV0.GET("hello", s.Hello.Handler)
