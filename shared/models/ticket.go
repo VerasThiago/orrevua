358b2ab72f1e432d242b16176e7bc648f1990748
verasthiago <github@verasthiago.com>
2022-12-06 01:45:12 -0300

    Add base64image

--- shared/models/ticket.go
+++ shared/models/ticket.go
@@ -1,24 +1,33 @@
 package models
 
 import (
+	b64 "encoding/base64"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/skip2/go-qrcode"
 	"gorm.io/gorm"
 )
 
 type Ticket struct {
 	gorm.Model
-	ID       string    `json:"id" gorm:"primary_key"`
-	OwnerID  string    `json:"ownerid"`
-	Name     string    `json:"name"`
-	Email    string    `json:"email" gorm:"unique"`
-	CPF      string    `json:"cpf" gorm:"unique"`
-	IsUsed   bool      `json:"isused" gorm:"default:false"`
-	UsedTime time.Time `json:"usedtime"`
+	ID          string    `json:"id" gorm:"primary_key"`
+	OwnerID     string    `json:"ownerid"`
+	Name        string    `json:"name"`
+	Email       string    `json:"email" gorm:"unique"`
+	CPF         string    `json:"cpf" gorm:"unique"`
+	IsUsed      bool      `json:"isused" gorm:"default:false"`
+	UsedTime    time.Time `json:"usedtime"`
+	Base64Image string    `json:"base64image"`
 }
 
 func (u *Ticket) BeforeCreate(tx *gorm.DB) (err error) {
 	u.ID = uuid.New().String()
+
+	qrCode, err := qrcode.Encode(u.CPF, qrcode.Medium, 256)
+	if err != nil {
+		return err
+	}
+	u.Base64Image = b64.StdEncoding.EncodeToString(qrCode)
 	return nil
 }
