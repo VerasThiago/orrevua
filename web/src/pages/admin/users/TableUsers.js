10eab6e044a83893fe674eefaecce4dfb1e992d3
Rodrigo Lima <limarodrigoo@outlook.com>
2022-12-06 22:30:55 -0300

    create TableUsers component

---
+++ web/src/pages/admin/users/TableUsers.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as AddBtn } from '../../../images/add_circle.svg';
+
+export default function TableUsers({ data }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 8;
+  const pageNumbers = [];
+  const numberOfPages = Math.ceil(data.length / usersPerPage);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [data]);
+
+  for (let i = 1; i <= numberOfPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginateNext = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+  const paginatePrevious = () => {
+    setCurrentPage((currentPage) => currentPage - 1);
+  };
+
+  const cpfMask = (value) => {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1');
+  };
+
+  return (
+    <div>
+      <table className="table border-dark">
+        <tbody>
+          <tr>
+            <th className="text-white fw-bold">Nome</th>
+            <th className="text-white fw-bold">E-mail</th>
+            <th className="text-white fw-bold">CPF</th>
+            <th></th>
+          </tr>
+          {data
+            .slice(currentPage * usersPerPage - usersPerPage, usersPerPage * currentPage)
+            .map((item, index) => (
+              <tr key={index}>
+                <td className="text-white">{item.name}</td>
+                <td className="text-white">{item.email}</td>
+                <td className="text-white">{cpfMask(item.cpf)}</td>
+                <td className="">
+                  <NavLink to={`/admin/users/${item.id}`}>
+                    <AddBtn />
+                  </NavLink>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <nav>
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <a className="page-link" onClick={paginatePrevious}>
+              <span role="button" aria-hidden="true">
+                &laquo;
+              </span>
+            </a>
+          </li>
+          {pageNumbers.map((num, index) => (
+            <li className={`page-item ${currentPage === num ? 'active' : ''}`} key={index}>
+              <span role="button" className="page-link" onClick={() => setCurrentPage(num)}>
+                {num}
+              </span>
+            </li>
+          ))}
+          <li className={`page-item ${currentPage === numberOfPages ? 'disabled' : ''}`}>
+            <a className="page-link" onClick={paginateNext}>
+              <span role="button" aria-hidden="true">
+                &raquo;
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
