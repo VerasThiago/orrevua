b2da9f76131aa1b4edb066a7fb7a130d24e12375
Rodrigo Lima <limarodrigoo@outlook.com>
2022-12-06 22:31:07 -0300

    Create list all users page

---
+++ web/src/pages/admin/users/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { apiRequest } from '../../../services/api';
+import TableUsers from './TableUsers';
+import alertMessage from '../../../components/alertMessage';
+import Loading from '../../../components/loading';
+
+export default function Users() {
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const keys = ['name', 'cpf', 'email'];
+
+  useEffect(() => {
+    setLoading(true);
+    apiRequest('login', 'login/v0/user/list', 'get')
+      .then(async (response) => {
+        const dataUsers = await response.json();
+        if (response.ok) {
+          setData(dataUsers.data);
+        } else {
+          if (dataUsers.message) alertMessage('error', dataUsers.message);
+        }
+      })
+      .catch(() => {
+        console.log('error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+    );
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (!data) {
+    return <h1>Error</h1>;
+  }
+  return (
+    <div>
+      <h1 className="text-white m-5 fs-2">Usuários</h1>
+      <div className="form-group">
+        <div className="input-group w-25 mx-5">
+          <input
+            className="rounded-pill form-control "
+            type="text"
+            placeholder="Buscar nome, e-mail ou cpf"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="m-5">
+        <TableUsers data={search(data)} />
+      </div>
+    </div>
+  );
+}
