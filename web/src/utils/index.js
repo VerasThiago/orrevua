c29ed828c5e3e92e8c630d91e106f9674334c177
Rodrigo Lima <limarodrigoo@outlook.com>
2022-12-08 03:26:27 -0300

    create function to format cpf to request

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -22,3 +22,7 @@ export function formatCpf(cpf) {
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1');
 }
+
+export function formatToRequestCpf(cpf) {
+  return cpf.split('.').join('').replace('-', '');
+}
