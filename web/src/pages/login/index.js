95d8adaaac0e24ff10830c79f2e666c26bb4a488
Rodrigo Cunha Lima <limarodrigoo@outlook.com>
2022-12-08 20:33:33 -0300

    Feat fix login page (#74)

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -1,98 +1,99 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './index.scss';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Input } from 'antd';
+import { ReactComponent as IconUser } from '../../images/user.svg';
+import { ReactComponent as IconPassword } from '../../images/password.svg';
+import InputIcon from '../../components/inputIcon';
 import { useNavigate, useLocation, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../App';
 import HomeSidebar from '../../components/homeSidebar';
+import alertMessage from '../../components/alertMessage';
 
 export default function Login() {
   const navigate = useNavigate();
   const location = useLocation();
   const { login } = useContext(AuthContext);
+  const [user, setUser] = useState({ email: '', password: '' });
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setUser({
+      ...user,
+      [event.target.name]: value
+    });
+  };
 
-  const onFinish = async (values) => {
-    const result = await login(values);
-    if (result) navigate('/tickets', { replace: true, state: { from: location } });
+  const onFinish = async (event) => {
+    event.preventDefault();
+    if (user.email === '') {
+      alertMessage('error', 'Insira seu e-mail!');
+    } else if (user.password.length < 6) {
+      alertMessage('error', 'Sua senha deve ter no mínimo 6 caracteres');
+    } else {
+      const result = await login(user);
+      if (result) navigate('/tickets', { replace: true, state: { from: location } });
+    }
   };
 
   return (
     <div className="row vh-100 m-0">
       <HomeSidebar />
       <div className="col-lg-6 col-md-12 bg-secondary d-flex flex-column align-items-center justify-content-center">
-        <div className="col-md-6">
-          <p className="login-title-forms fw-bold">Entrar</p>
-        </div>
-        <div className="col-md-6">
-          <p className="login-greeting-forms fw-bold">
-            Sentimos sua falta! Faça o login para começar
-          </p>
-        </div>
-        <div className="col-md-6">
-          <Form
-            name="login"
-            initialValues={{
-              remember: true
-            }}
-            onFinish={onFinish}>
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'Email é obrigatório'
-                },
-                {
-                  type: 'email',
-                  message: 'Email deve ser um email válido'
-                }
-              ]}>
-              <Input
-                className="rounded-pill p-3"
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="E-mail"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Senha é obrigatória'
-                },
-                {
-                  min: 6,
-                  message: 'Senha deve conter pelo menos 6 caracteres'
-                }
-              ]}>
-              <Input
-                className="rounded-pill p-3"
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Senha"
-              />
-            </Form.Item>
-            <div className="d-flex justify-content-end pb-5">
-              <NavLink
-                to={'/forgot_password'}
-                className="login-forgot-password fw-bold text-decoration-none">
-                Esqueceu a senha?
-              </NavLink>
-            </div>
-            <Form.Item>
-              <button type="submit" className="btn btn-primary w-100 rounded-pill py-3">
-                Entrar
-              </button>
-              <div className="d-flex justify-content-center p-5">
-                <p className="login-signup-forms">
-                  Não tem conta?
-                  <NavLink to={'/signup'} className="mx-2 text-decoration-none fw-bold">
-                    Cadastre-se
-                  </NavLink>
-                </p>
+        <div className="px-4" style={{ maxWidth: '450px' }}>
+          <div>
+            <p className="fs-2">Entrar</p>
+          </div>
+          <div>
+            <p>Sentimos sua falta! Faça o login para começar</p>
+          </div>
+          <div>
+            <form id="login" onSubmit={onFinish}>
+              <div className="mb-3">
+                <InputIcon
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="form-control rounded-pill p-3 bg-dark"
+                  aria-describedby="email"
+                  placeholder="E-mail"
+                  onChange={handleChange}
+                  icon={<IconUser />}
+                />
               </div>
-            </Form.Item>
-          </Form>
+              <div>
+                <InputIcon
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="form-control rounded-pill p-3 bg-dark"
+                  aria-describedby="password"
+                  placeholder="Senha"
+                  onChange={handleChange}
+                  icon={<IconPassword />}
+                />
+              </div>
+              <div className="d-flex justify-content-end my-3">
+                <NavLink
+                  to={'/forgot_password'}
+                  className="login-forgot-password text-decoration-none">
+                  Esqueceu a senha?
+                </NavLink>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="btn btn-primary w-100 rounded-pill py-3 mt-5 fw-bold">
+                  Entrar
+                </button>
+                <div className="d-flex justify-content-center p-5">
+                  <p className="login-signup-forms">
+                    Não tem conta?
+                    <NavLink to={'/signup'} className="mx-2 text-decoration-none fw-bold">
+                      Cadastre-se
+                    </NavLink>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
