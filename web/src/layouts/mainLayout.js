0a0d4ae6dabb12e104e663323ca042e81b09f9df
diogoftp <31069709+diogoftp@users.noreply.github.com>
2022-12-08 02:11:36 -0300

    fixed main layout for small screens

--- web/src/layouts/mainLayout.js
+++ web/src/layouts/mainLayout.js
@@ -3,10 +3,10 @@ import Menu from '../components/menu';
 export default function MainLayout({ children }) {
   return (
     <div className="row m-0 p-0 vh-100">
-      <div className="col-2 p-0 m-0">
+      <div className="col-lg-2 p-0 m-0">
         <Menu />
       </div>
-      <div className="col-10 m-0 p-4">{children}</div>
+      <div className="col-lg-10 m-0 p-4">{children}</div>
     </div>
   );
 }
