1a165bdcc05498e4efd75d2d1802eb65bf5816b0
diogoftp <31069709+diogoftp@users.noreply.github.com>
2022-12-11 01:22:14 -0300

    improved menu for admins

--- web/src/components/menu.js
+++ web/src/components/menu.js
@@ -4,26 +4,34 @@ import { ReactComponent as LogoIcon } from '../images/logo.svg';
 import { ReactComponent as LogoutIcon } from '../images/logout.svg';
 import { ReactComponent as TicketIcon } from '../images/ticket.svg';
 import { ReactComponent as SettingsIcon } from '../images/settings.svg';
+import { ReactComponent as UserIcon } from '../images/user.svg';
 import { AuthContext } from '../App';
 
 export default function Menu() {
-  const { logout } = useContext(AuthContext);
+  const { logout, userData } = useContext(AuthContext);
   const location = useLocation();
 
   const menuItems = [
     {
       title: 'Meus ingressos',
       icon: <TicketIcon />,
-      path: location.pathname.includes('/admin') ? '/admin/users' : '/tickets',
-      active: location.pathname.includes('/tickets') || location.pathname.includes('/admin'),
-      hidden: false
+      path: '/tickets',
+      active: location.pathname.includes('/tickets'),
+      admin: false
+    },
+    {
+      title: 'Usuários',
+      icon: <UserIcon />,
+      path: '/admin/users',
+      active: location.pathname.includes('/admin/users'),
+      admin: true
     },
     {
       title: 'Configurações',
       icon: <SettingsIcon />,
       path: '/settings',
       active: location.pathname.includes('/settings'),
-      hidden: location.pathname.includes('/admin')
+      admin: false
     }
   ];
 
@@ -34,24 +42,28 @@ export default function Menu() {
           <LogoIcon />
           <span className="fw-bold fs-2">orrevuá</span>
         </div>
-        <div className="d-flex flex-column align-items-center gap-3">
-          {menuItems.map((menuItem, index) => (
-            <div
-              key={`menu_${index}`}
-              className={`${menuItem.active && 'bg-primary'} ${
-                menuItem.hidden && 'visually-hidden'
-              } px-4 py-2 rounded-pill`}
-              role="button">
+        <nav className="navbar d-flex flex-column align-items-center gap-3">
+          {menuItems.map((menuItem, index) => {
+            if (menuItem.admin && !userData().User.isadmin === true) return null;
+
+            return (
               <Link
+                key={`menu_${index}`}
                 to={menuItem.path}
                 state={{ from: location }}
                 className="text-decoration-none text-white">
-                {menuItem.icon}
-                <span className="ms-2">{menuItem.title}</span>
+                <div
+                  className={`${menuItem.active && 'bg-primary'} px-4 py-2 rounded-pill`}
+                  role="button">
+                  <span className="ms-2">
+                    {menuItem.icon}
+                    <span className="ms-2">{menuItem.title}</span>
+                  </span>
+                </div>
               </Link>
-            </div>
-          ))}
-        </div>
+            );
+          })}
+        </nav>
       </div>
       <div className="d-flex justify-content-center mb-5" role="button" onClick={() => logout()}>
         <LogoutIcon className="me-2" />
