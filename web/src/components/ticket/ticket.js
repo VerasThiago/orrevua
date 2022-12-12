1071d1389cded6c4849b3a336e474ec51354d803
Diogo <31069709+diogoftp@users.noreply.github.com>
2022-12-11 23:53:12 -0300

    Feat/responsive ticket (#96)

--- web/src/components/ticket/ticket.js
+++ web/src/components/ticket/ticket.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { ReactComponent as IconMenu } from '../../images/qr_code.svg';
-import ticketImg from './ticket-img.png';
-import UserTicketHeader from './userTicketHeader';
+import TicketMobile from './ticketMobile';
+import TicketDesktop from './ticketDesktop';
 
 export default function Ticket({ ticket, user, reloadTickets, hideDelete }) {
   const [qrCodeVisible, setQrCodeVisible] = useState(false);
@@ -11,40 +10,16 @@ export default function Ticket({ ticket, user, reloadTickets, hideDelete }) {
   };
 
   return (
-    <div className="col-lg-3 col-md-12 rounded-3 py-0 px-0" style={{ backgroundColor: '#1f1f1f' }}>
-      <UserTicketHeader
-        owner={user}
+    <>
+      <TicketDesktop
         ticket={ticket}
+        user={user}
         reloadTickets={reloadTickets}
         hideDelete={hideDelete}
+        qrCodeVisible={qrCodeVisible}
+        showQrCode={showQrCode}
       />
-      {qrCodeVisible === true ? (
-        <div className="text-center">
-          <img
-            className="img-fluid"
-            src={`data:image/png;base64, ${ticket.base64image}`}
-            alt="QR Code"
-          />
-        </div>
-      ) : (
-        <img src={ticketImg} alt="ticket image" className="img-fluid" />
-      )}
-
-      <div className="p-3">
-        <p className="fw-bold">Despedida do Veras</p>
-        <span>23 de Dezembro de 2022 às 20h</span>
-        <br />
-        <span>
-          Espaço Mocambo, St. de Habitações Individuais Norte Trecho 1 - Lago Norte, Brasília - DF,
-          71560-100
-        </span>
-      </div>
-      <div className="p-3 pt-4">
-        <button className="btn btn-primary py-2 w-100" onClick={showQrCode}>
-          <IconMenu className="me-2" />
-          <small>QR Code</small>
-        </button>
-      </div>
-    </div>
+      <TicketMobile ticket={ticket} qrCodeVisible={qrCodeVisible} showQrCode={showQrCode} />
+    </>
   );
 }
---
+++ web/src/components/ticket/ticketDesktop.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { ReactComponent as IconMenu } from '../../images/qr_code.svg';
+import ticketImg from './ticket-img.png';
+import UserTicketHeader from './userTicketHeader';
+
+export default function TicketDesktop({
+  ticket,
+  user,
+  reloadTickets,
+  hideDelete,
+  qrCodeVisible,
+  showQrCode
+}) {
+  return (
+    <div className="col-lg-3 col-md-12 rounded-3 py-0 px-0 bg-dark d-none d-lg-block">
+      <UserTicketHeader
+        owner={user}
+        ticket={ticket}
+        reloadTickets={reloadTickets}
+        hideDelete={hideDelete}
+      />
+      {qrCodeVisible === true ? (
+        <div className="text-center">
+          <img
+            className="img-fluid"
+            src={`data:image/png;base64, ${ticket.base64image}`}
+            alt="QR Code"
+          />
+        </div>
+      ) : (
+        <div className="d-flex align-items-center" style={{ minHeight: '256px' }}>
+          <img src={ticketImg} alt="ticket image" className="img-fluid" />
+        </div>
+      )}
+
+      <div className="p-3">
+        <p className="fw-bold">Despedida do Veras</p>
+        <small>23 de Dezembro de 2022 às 20h</small>
+        <br />
+        <small>
+          Espaço Mocambo, St. de Habitações Individuais Norte Trecho 1 - Lago Norte, Brasília - DF,
+          71560-100
+        </small>
+      </div>
+      <div className="p-3 pt-4">
+        <button className="btn btn-primary py-2 w-100" onClick={showQrCode}>
+          {!qrCodeVisible && <IconMenu className="me-2" />}
+          <small>{qrCodeVisible ? 'Voltar' : 'QR Code'}</small>
+        </button>
+      </div>
+    </div>
+  );
+}
