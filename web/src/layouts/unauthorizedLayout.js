35be2e3b3b73c8c7c0d8ea211d347ef1d9a0e27f
diogoftp <31069709+diogoftp@users.noreply.github.com>
2022-12-13 19:22:50 -0300

    fixed unauthorized layout

--- web/src/layouts/unauthorizedLayout.js
+++ web/src/layouts/unauthorizedLayout.js
@@ -1,13 +1,13 @@
-import './mainLayout.scss';
+import './unauthorizedLayout.scss';
 import HomeSidebar from '../components/homeSidebar';
 
 export default function UnauthorizedLayout({ children }) {
   return (
-    <div className="row m-0 p-0 vh-100 bg-dark">
-      <div className="col-lg-6 m-0 p-0">
+    <div className="row m-0 p-0 vh-100">
+      <div className="col-lg-6 m-0 p-0 mb-5 mb-lg-0">
         <HomeSidebar />
       </div>
-      <div className="col-lg-6 m-0 px-4 h-100 bg-secondary layout-container">{children}</div>
+      <div className="col-lg-6 m-0 px-4 bg-secondary unauthorized-layout-container">{children}</div>
     </div>
   );
 }
