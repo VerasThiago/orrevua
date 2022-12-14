2b611338199ba3f777b8b0112beac41277266574
diogoftp <31069709+diogoftp@users.noreply.github.com>
2022-12-13 21:32:21 -0300

    fixed menu on big heights

--- web/src/layouts/mainLayout.js
+++ web/src/layouts/mainLayout.js
@@ -3,11 +3,13 @@ import './mainLayout.scss';
 
 export default function MainLayout({ children }) {
   return (
-    <div className="row m-0 p-0 vh-100 bg-dark">
+    <div className="row m-0 p-0 min-vh-100 h-100 bg-dark">
       <div className="col-lg-2 p-0 m-0">
         <Menu />
       </div>
-      <div className="col-lg-10 m-0 p-4 bg-secondary h-100 layout-container">{children}</div>
+      <div className="col-lg-10 m-0 p-4 bg-secondary min-vh-100 h-100 layout-container">
+        {children}
+      </div>
     </div>
   );
 }
