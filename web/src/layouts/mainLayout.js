9ebd4267471cfefc8e6289c88be08994431742d2
Diogo <31069709+diogoftp@users.noreply.github.com>
2022-12-15 19:14:37 -0300

    fixed menu max width on big screens (#117)

--- web/src/layouts/mainLayout.js
+++ web/src/layouts/mainLayout.js
@@ -4,7 +4,7 @@ import './mainLayout.scss';
 export default function MainLayout({ children }) {
   return (
     <div className="row m-0 p-0 min-vh-100 h-100 bg-dark">
-      <div className="col-lg-2 p-0 m-0">
+      <div className="col-lg-2 p-0 m-0 layout-header">
         <Menu />
       </div>
       <div className="col-lg-10 m-0 p-4 bg-secondary min-vh-100 h-100 layout-container">
